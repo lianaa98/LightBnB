2bd123578e0d2c07c33af8a1712fdf0105435f3a
lianaa98 <liana.kan98@gmail.com>
2023-05-04 11:54:10 -0700

    reservations function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -95,7 +95,22 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT reservations.id, properties.title, reservations.start_date, properties.cost_per_night, avg(property_reviews.rating), properties.thumbnail_photo_url
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON property_reviews.property_id = properties.id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.title, properties.cost_per_night, properties.thumbnail_photo_url
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
 };
 
 /// Properties
---
+++ LightBnB_WebApp-master/server.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const express = require("express");
+const cookieSession = require("cookie-session");
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1"],
+  })
+);
+
+const apiRoutes = require("./routes/apiRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+// /api/endpoints
+app.use("/api", apiRoutes);
+
+// /user/endpoints
+app.use("/users", userRoutes);
+
+app.get("/test", (req, res) => {
+  res.send("🤗");
+});
+
+app.listen(port, (err) => {
+  console.log(err || `listening on port ${port} 😎`);
+});
